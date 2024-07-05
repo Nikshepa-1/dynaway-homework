e95994a9e8d0921a7e17460a96f9e1babcd10405
Nikshepa . <nikshepa@ad.infosys.com>
2024-07-05 12:04:30 +0530

    Task 2 Solution commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { Asset } from '../shared/models/asset.model'
 import { AssetService } from '../shared/services/asset.service'
+import { error } from 'console'
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,22 @@ import { AssetService } from '../shared/services/asset.service'
 })
 export class HomePage {
   assets: Asset[] = []
+  showError = false;
+  alertButtons = ['Close'];
 
   constructor(private assetService: AssetService) {}
 
   ionViewWillEnter(): void {
     this.assets = []
-    this.assetService.getAll().subscribe(assets =>  
+    this.assetService.getAll().subscribe((assets) => {
       this.assets = assets.data
-    )
+    },
+    err => {
+      this.setOpen(true)
+    });
+    }
+
+    setOpen(isOpen: boolean) {
+      this.showError = isOpen;
     }
   }
